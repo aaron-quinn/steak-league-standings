63f9b49ba569d0e9ce552163b93303bf511339e0
Aaron Quinn <awquinn33@gmail.com>
2022-09-07 23:15:10 -0500

    Added data for managers
    An initial structure anyway

---
+++ src/data/managers.js
@@ -0,0 +1,470 @@
+export default function getManagers() {
+  return [
+    {
+      name: 'Aaron Quinn',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0002',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0002',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'John McKechnie',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0009',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0009',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: 'Andrew Parr',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0010',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0010',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'D.J. Trainor',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0004',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0004',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'Ryan Pohle',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0003',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'Brigham Heyn',
+      teams: {
+        2021: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0003',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'Harry Thompson',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0011',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0011',
+          steak: true,
+          champion: true,
+        },
+      },
+    },
+    {
+      name: 'Shannon McKeown',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0012',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0012',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'Chris Benzine',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0013',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Au Poivre',
+          teamID: '0013',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'Kurt Kroll',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0005',
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0005',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: `Kevin O'Brien`,
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0001',
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0001',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'Nick Rawlings',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0006',
+        },
+      },
+    },
+    {
+      name: 'Mario Puig',
+      teams: {
+        2021: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0006',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'Alan Seslowski',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0014',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0014',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: 'Erik Caturia',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0007',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0007',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: 'Jake Letarski',
+      teams: {
+        2022: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0008',
+          steak: false,
+        },
+        2021: {
+          league: 'Madison',
+          division: 'Filet Mignon',
+          teamID: '0008',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: 'Paul Martinez',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0001',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0001',
+        },
+      },
+    },
+    {
+      name: 'Peter Schoenke',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0004',
+          steak: false,
+        },
+        2021: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0004',
+          steak: false,
+        },
+      },
+    },
+    {
+      name: 'Ken Crites',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0012',
+          steak: false,
+        },
+        2021: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0012',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: 'Len Hochberg',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0006',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0006',
+        },
+      },
+    },
+    {
+      name: 'Tim Schuler',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0009',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0009',
+        },
+      },
+    },
+    {
+      name: 'Joe Bartel',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0010',
+          steak: false,
+        },
+        2021: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0010',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: 'Mike Doria',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0003',
+          steak: false,
+        },
+        2021: {
+          league: 'LA',
+          division: 'Taylors',
+          teamID: '0003',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: 'Chris Liss',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0005',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0005',
+        },
+      },
+    },
+    {
+      name: 'Herb Ilk',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0011',
+          steak: false,
+        },
+        2021: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0011',
+          steak: true,
+        },
+      },
+    },
+    {
+      name: 'Scott Jenstad',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0008',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0008',
+        },
+      },
+    },
+    {
+      name: 'Jeff Erickson',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0007',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0007',
+        },
+      },
+    },
+    {
+      name: 'Jason Thornbury',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0013',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0013',
+        },
+      },
+    },
+    {
+      name: 'Erik Siegrist',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0002',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0002',
+        },
+      },
+    },
+    {
+      name: 'Erik Siegrist',
+      teams: {
+        2022: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0014',
+        },
+        2021: {
+          league: 'LA',
+          division: 'Tornado Room',
+          teamID: '0014',
+        },
+      },
+    },
+  ];
+}
