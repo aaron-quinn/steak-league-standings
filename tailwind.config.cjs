856f8d02b3cfe5c724bf0af8849e267b448627b6
Aaron Quinn <awquinn33@gmail.com>
2022-09-07 23:14:48 -0500

    Adding Urbanist as custom font
    Need to remember to get rid of font weights that I don't end up using.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Urbanist', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
